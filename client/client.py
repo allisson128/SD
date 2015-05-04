54a8425cfdedd756f38b2f2df2fd8751abfa94d1
SidneyLima <sidneyflimasfl@gmail.com>
2015-05-04 16:40:28 -0300

    Update client.py

--- client/client.py
+++ client/client.py
@@ -1,33 +1,42 @@
 from httpClientRequest import HttpClientRequest
 from sys import argv
-    
+
 #------------------------------------------------    
 #teste criado para avaliar quantidade de argumentos
 #------------------------------------------------
 def verificarQuantidadeArgumentos():
-    if(len(argv) != 2):
+    n = len(argv)
+    if(n != 2 and n != 3):
         print("Insira URL")
-        return False
+        return -1
+    if(n == 3 and argv[1] != 'post'):
+        print("Apenas sera permitido \"post\" antes da URL")
+        return -1
     
-    return True
+    return n
 
 #------------------------------------------------    
 #metodo MAIN
 #------------------------------------------------
 
 def main():
-    if(not verificarQuantidadeArgumentos()):
+    n = verificarQuantidadeArgumentos()
+    if(n == -1):
         exit()
 
     httpClientRequest = HttpClientRequest()
     
-    getResponse = httpClientRequest.doGetRequest(argv[1])
-    #exibe toda a resposta get
-    print(getResponse)
-    html = httpClientRequest.htmlFromGETResponse(getResponse)
-    #exibe apenas a pagina html
-    #print(httpClientRequest.htmlFromGETResponse(getResponse))
-    with open('output.html','w') as f:
-        f.write(html)
+    if(n == 2):
+        response = httpClientRequest.doGetRequest(argv[1])
+        #exibe toda a resposta get
+        print(response)
+        html = httpClientRequest.htmlFromGETResponse(response)
+        #exibe apenas a pagina html
+        #print(httpClientRequest.htmlFromGETResponse(response))
+        with open('output.html','w') as f:
+            f.write(html)
+    else:
+        response = httpClientRequest.doPostRequest(argv[2])
+        print(response)
 
 main()
