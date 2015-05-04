a08a5a794975bd675a807081bd42118c8c26dc22
SidneyLima <sidneyflimasfl@gmail.com>
2015-05-04 16:40:52 -0300

    Update httpClientRequest.py

--- client/httpClientRequest.py
+++ client/httpClientRequest.py
@@ -88,14 +88,14 @@ class HttpClientRequest:
         #enviando mensagem via socket
         print('Enviando mensagem via socket...')
         self.__log.writeLog('Enviando mensagem via socket...')
-        self.__sendingGETMessage(s,getMessage)
+        self.__sendingMessage(s,getMessage)
         print('\t::Mensagem enviada com sucesso!')
         self.__log.writeLog('\t::Mensagem enviada com sucesso!')
 
         #recebendo mensagem do servidor
         print('Recebendo mensagem do servidor...')
         self.__log.writeLog('Recebendo mensagem do servidor...')
-        receivedMessage = self.__receivingGETMessage(s)
+        receivedMessage = self.__receivingMessage(s)
         print('\t::Mensagem recebida com sucesso de Host \"'+socketInfo[0]+'\" Porta \"'+str(socketInfo[1])+'\"!')
         print('\t::MENSAGEM RECEBIDA\n\n****\n'+receivedMessage+'\n****')
         self.__log.writeLog('\t::Mensagem recebida com sucesso de Host \"'+socketInfo[0]+'\" Porta \"'+str(socketInfo[1])+'\"!')
@@ -103,6 +103,70 @@ class HttpClientRequest:
 
         return receivedMessage
 
+    #Realiza o metodo POST do HTTP
+    def doPostRequest(self,url):
+        print('Iniciando requisição POST para URL \"' + url + '\"...')
+        self.__log.writeLog('Iniciando requisição POST para URL \"' + url + '\"...')
+
+        #captura URL
+        print('Parsing URL...')
+        self.__log.writeLog('Parsing URL...')
+        
+        urlParse  = self.__getURLParsed(url)
+
+        #captura IP remoto e porta
+        print('Obtendo Host IP e numero da porta para conexao...')
+        self.__log.writeLog('Obtendo Host IP e numero da porta para conexao...')
+        
+        socketInfo = self.__getIPPortHost(urlParse)
+
+        print('\t::IP para host \"' + urlParse.getHost() + '\" = ' + socketInfo[0] + '!')
+        print('\t::Porta da conexao = ' + str(socketInfo[1]) + '!')
+        self.__log.writeLog('\t::IP para host \"' + urlParse.getHost() + '\" = ' + socketInfo[0] + '!')
+        self.__log.writeLog('\t::Porta da conexao = ' + str(socketInfo[1]) + '!')
+        
+        #obtendo socket
+        print('Criando socket TCP...')
+        self.__log.writeLog('Criando socket TCP...')
+        
+        s = self.__openTCPSocketIPv4()
+        
+        print('\t::Socket criado com sucesso!')
+        self.__log.writeLog('\t::Socket criado com sucesso!')
+        
+        #iniciando conexao
+        print('Iniciando conexao...')
+        self.__log.writeLog('Iniciando conexao...')
+        
+        self.__beginSocketConnection(s, socketInfo)
+        
+        print('\t::Conexao iniciada em IP \"'+socketInfo[0]+'\" e Porta \"'+str(socketInfo[1])+'\"!')
+        self.__log.writeLog('\t::Conexao iniciada em IP \"'+socketInfo[0]+'\" e Porta \"'+str(socketInfo[1])+'\"!')
+        
+        #montando mensagem GET
+        print('Montando mensagem POST...')
+        self.__log.writeLog('Montando mensagem POST...')
+        message = self.__createPOSTMessageRequest(urlParse)
+        print('\t::MENSAGEM POST\n****\n'+message+'\n****')
+        self.__log.writeLog('\t::MENSAGEM POST\n****\n'+message+'\n****')
+        
+        #enviando mensagem via socket
+        print('Enviando mensagem via socket...')
+        self.__log.writeLog('Enviando mensagem via socket...')
+        self.__sendingMessage(s,message)
+        print('\t::Mensagem enviada com sucesso!')
+        self.__log.writeLog('\t::Mensagem enviada com sucesso!')
+
+        #recebendo mensagem do servidor
+        print('Recebendo mensagem do servidor...')
+        self.__log.writeLog('Recebendo mensagem do servidor...')
+        receivedMessage = self.__receivingMessage(s)
+        print('\t::Mensagem recebida com sucesso de Host \"'+socketInfo[0]+'\" Porta \"'+str(socketInfo[1])+'\"!')
+        print('\t::MENSAGEM RECEBIDA\n\n****\n'+receivedMessage+'\n****')
+        self.__log.writeLog('\t::Mensagem recebida com sucesso de Host \"'+socketInfo[0]+'\" Porta \"'+str(socketInfo[1])+'\"!')
+        self.__log.writeLog('\t::MENSAGEM RECEBIDA\n\n****\n'+receivedMessage+'\n****')
+
+        return receivedMessage
 
     #--------------------------------------------------
     # METODOS PRIVADOS
@@ -148,14 +212,24 @@ class HttpClientRequest:
 
     #monta mensagem GET
     def __createGETMessageRequest(self,urlParse):
-        get  = 'GET /{0} HTTP/1.0'.format(urlParse.getURLRelativa()) + HttpClientRequest.__saltoLinha
+        get  = 'GET /{0} HTTP/1.0'.format(urlParse.getURLRelativaGET()) + HttpClientRequest.__saltoLinha
         host = 'Host: {0}:{1}'.format(urlParse.getHost(), urlParse.getPorta()) + HttpClientRequest.__saltoLinha
         conexao = 'Connection: keep-alive'
         
         return get + host + conexao
 
+    #monta mensagem GET
+    def __createPOSTMessageRequest(self,urlParse):
+        post  = 'POST /{0} HTTP/1.0'.format(urlParse.getURLRelativa()) + HttpClientRequest.__saltoLinha
+        host = 'Host: {0}:{1}'.format(urlParse.getHost(), urlParse.getPorta()) + HttpClientRequest.__saltoLinha
+        conexao = 'Connection: keep-alive' + HttpClientRequest.__saltoLinha
+        parametros = urlParse.getParametrosURL() + HttpClientRequest.__saltoLinha + HttpClientRequest.__saltoLinha
+        
+        return post + host + conexao + parametros
+
+
     #enviando mensagem ao servidor via socket
-    def __sendingGETMessage(self,s,getMessage):
+    def __sendingMessage(self,s,getMessage):
         try:
             s.sendall(getMessage.encode('ascii'))
         except socket.error as msg:
@@ -163,7 +237,7 @@ class HttpClientRequest:
             sys.exit()
 
     #recebendo mensagem via socket do servidor
-    def __receivingGETMessage(self,s):
+    def __receivingMessage(self,s):
         try:
             return s.recv(HttpClientRequest.__bufferSize).decode()
         except socket.error as msg:
